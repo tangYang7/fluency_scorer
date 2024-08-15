59e5aa6fc00e28ec6499e934d659a3c0bc07cc68
tangYang7 <114934655+tangYang7@users.noreply.github.com>
2024-08-15 23:58:02 +0800

    Update run.sh

--- prep_data/run.sh
+++ prep_data/run.sh
@@ -2,12 +2,12 @@ SO762_dir=../speechocean762
 score_json_file='scores.json'
 feat_dir=../data
 
-# python3 gen_seq_data_utt.py $SO762_dir $score_json_file
-# python3 gen_seq_acoustic_feat.py $SO762_dir --feat_dir $feat_dir
+python3 gen_seq_data_utt.py $SO762_dir $score_json_file
+python3 gen_seq_acoustic_feat.py $SO762_dir --feat_dir $feat_dir
 
 # generate cluster_id by keans
 python3 train_kmeans.py $SO762_dir --feat_dir $feat_dir
-# python3 kmeans_metric.py $SO762_dir --feat_dir $feat_dir
+python3 kmeans_metric.py $SO762_dir --feat_dir $feat_dir
 
 # optional: try wav2vec force alignment results as cluster id
 # python3 gen_ctc_force_align.py $SO762_dir --feat_dir $feat_dir
